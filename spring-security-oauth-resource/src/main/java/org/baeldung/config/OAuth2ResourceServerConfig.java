3adbeb3592a2a36d8e38d131d380d251d99e618e
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2016-09-27 16:50:42 +0700

    comment old tokenStore and usage of jdbc tokenStore

--- spring-security-oauth-resource/src/main/java/org/baeldung/config/OAuth2ResourceServerConfig.java
+++ spring-security-oauth-resource/src/main/java/org/baeldung/config/OAuth2ResourceServerConfig.java
@@ -2,6 +2,8 @@ package org.baeldung.config;
 
 import java.io.IOException;
 
+import javax.sql.DataSource;
+
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +13,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -18,22 +21,22 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @PropertySource({ "classpath:persistence.properties" })
 @EnableResourceServer
 public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-    @Autowired
-    private Environment env;
+	@Autowired
+	private Environment env;
 
-    //
+	//
 
-    @Override
-    public void configure(final HttpSecurity http) throws Exception {
-        // @formatter:off
+	@Override
+	public void configure(final HttpSecurity http) throws Exception {
+		// @formatter:off
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).and().authorizeRequests()
 				.anyRequest().authenticated();
 		// .requestMatchers().antMatchers("/foos/**","/bars/**")
@@ -49,68 +52,70 @@ public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
 		// and #oauth2.hasScope('write') and hasRole('ROLE_ADMIN')")
 		;
 		// @formatter:on
-    }
+	}
 
-    // Remote token service
-    /*
-    @Primary
-    @Bean
-    public RemoteTokenServices tokenService() {
-        final RemoteTokenServices tokenService = new RemoteTokenServices();
-        tokenService.setCheckTokenEndpointUrl("http://localhost:8081/spring-security-oauth-server/oauth/check_token");
-        tokenService.setClientId("fooClientIdPassword");
-        tokenService.setClientSecret("secret");
-        return tokenService;
-    }
-    */
+	// Remote token service
+	/*
+	 * @Primary
+	 * 
+	 * @Bean public RemoteTokenServices tokenService() { final
+	 * RemoteTokenServices tokenService = new RemoteTokenServices();
+	 * tokenService.setCheckTokenEndpointUrl(
+	 * "http://localhost:8081/spring-security-oauth-server/oauth/check_token");
+	 * tokenService.setClientId("fooClientIdPassword");
+	 * tokenService.setClientSecret("secret"); return tokenService; }
+	 */
 
-    // JWT token store
+	// JWT token store
 
-    @Override
-    public void configure(final ResourceServerSecurityConfigurer config) {
-        config.tokenServices(tokenServices());
-    }
+	@Override
+	public void configure(final ResourceServerSecurityConfigurer config) {
+		config.tokenServices(tokenServices());
+	}
+/*
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+*/
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		// converter.setSigningKey("123");
+		final Resource resource = new ClassPathResource("public.txt");
+		String publicKey = null;
+		try {
+			publicKey = IOUtils.toString(resource.getInputStream());
+		} catch (final IOException e) {
+			throw new RuntimeException(e);
+		}
+		converter.setVerifierKey(publicKey);
+		return converter;
+	}
 
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
-    }
+	@Bean
+	@Primary
+	public DefaultTokenServices tokenServices() {
+		final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+		defaultTokenServices.setTokenStore(tokenStore());
+		return defaultTokenServices;
+	}
 
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        // converter.setSigningKey("123");
-        final Resource resource = new ClassPathResource("public.txt");
-        String publicKey = null;
-        try {
-            publicKey = IOUtils.toString(resource.getInputStream());
-        } catch (final IOException e) {
-            throw new RuntimeException(e);
-        }
-        converter.setVerifierKey(publicKey);
-        return converter;
-    }
+	// JDBC token store configuration
 
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-        final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        return defaultTokenServices;
-    }
+	@Bean
+	public DataSource dataSource() {
+		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.user"));
+		dataSource.setPassword(env.getProperty("jdbc.pass"));
+		return dataSource;
+	}
 
-    // JDBC token store configuration
+	@Bean
+	public TokenStore tokenStore() {
+		return new JdbcTokenStore(dataSource());
+	}
 
-    /*
-     * @Bean public DataSource dataSource() { final DriverManagerDataSource
-     * dataSource = new DriverManagerDataSource();
-     * dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-     * dataSource.setUrl(env.getProperty("jdbc.url"));
-     * dataSource.setUsername(env.getProperty("jdbc.user"));
-     * dataSource.setPassword(env.getProperty("jdbc.pass")); return dataSource;
-     * }
-     *
-     * @Bean public TokenStore tokenStore() { return new
-     * JdbcTokenStore(dataSource()); }
-     */
 }
--- spring-security-oauth-server/src/main/java/org/baeldung/config/OAuth2AuthorizationServerConfig.java
+++ spring-security-oauth-server/src/main/java/org/baeldung/config/OAuth2AuthorizationServerConfig.java
@@ -2,6 +2,8 @@ package org.baeldung.config;
 
 import java.util.Arrays;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +14,10 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -22,8 +28,8 @@ import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 @Configuration
@@ -31,29 +37,29 @@ import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFacto
 @EnableAuthorizationServer
 public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
-    @Autowired
-    private Environment env;
+	@Autowired
+	private Environment env;
 
-    @Autowired
-    @Qualifier("authenticationManagerBean")
-    private AuthenticationManager authenticationManager;
+	@Autowired
+	@Qualifier("authenticationManagerBean")
+	private AuthenticationManager authenticationManager;
 
-    @Value("classpath:schema.sql")
-    private Resource schemaScript;
+	@Value("classpath:schema.sql")
+	private Resource schemaScript;
 
-    //
+	//
 
-    @Override
-    public void configure(final AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-        oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
-    }
+	@Override
+	public void configure(final AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+		oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+	}
 
-    @Override
-    public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {// @formatter:off
-		clients
-				// .jdbc(dataSource())
-				.inMemory().withClient("sampleClientId").authorizedGrantTypes("implicit")
-				.scopes("read", "write", "foo", "bar").autoApprove(false).accessTokenValiditySeconds(3600)
+	@Override
+	public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {// @formatter:off
+		clients.jdbc(dataSource())
+				// .inMemory()
+				.withClient("sampleClientId").authorizedGrantTypes("implicit").scopes("read", "write", "foo", "bar")
+				.autoApprove(false).accessTokenValiditySeconds(3600)
 
 				.and().withClient("fooClientIdPassword").secret("secret")
 				.authorizedGrantTypes("password", "authorization_code", "refresh_token").scopes("foo", "read", "write")
@@ -67,67 +73,75 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
 		;
 	} // @formatter:on
 
-    @Override
-    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        // @formatter:off
+	@Override
+	public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		// @formatter:off
 		final TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
 		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
 		endpoints.tokenStore(tokenStore())
 				// .accessTokenConverter(accessTokenConverter())
 				.tokenEnhancer(tokenEnhancerChain).authenticationManager(authenticationManager);
 		// @formatter:on
-    }
+	}
+/*
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+*/
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		// converter.setSigningKey("123");
+		final KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("mytest.jks"),
+				"mypass".toCharArray());
+		converter.setKeyPair(keyStoreKeyFactory.getKeyPair("mytest"));
+		return converter;
+	}
 
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
-    }
+	@Bean
+	@Primary
+	public DefaultTokenServices tokenServices() {
+		final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+		defaultTokenServices.setTokenStore(tokenStore());
+		defaultTokenServices.setSupportRefreshToken(true);
+		return defaultTokenServices;
+	}
 
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        // converter.setSigningKey("123");
-        final KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("mytest.jks"), "mypass".toCharArray());
-        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("mytest"));
-        return converter;
-    }
+	@Bean
+	public TokenEnhancer tokenEnhancer() {
+		return new CustomTokenEnhancer();
+	}
 
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-        final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        defaultTokenServices.setSupportRefreshToken(true);
-        return defaultTokenServices;
-    }
+	// JDBC token store configuration
 
-    @Bean
-    public TokenEnhancer tokenEnhancer() {
-        return new CustomTokenEnhancer();
-    }
+	@Bean
+	public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
+		final DataSourceInitializer initializer = new DataSourceInitializer();
+		initializer.setDataSource(dataSource);
+		initializer.setDatabasePopulator(databasePopulator());
+		return initializer;
+	}
 
-    // JDBC token store configuration
+	private DatabasePopulator databasePopulator() {
+		final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		populator.addScript(schemaScript);
+		return populator;
+	}
 
-    /*
-     * @Bean public DataSourceInitializer dataSourceInitializer(final DataSource
-     * dataSource) { final DataSourceInitializer initializer = new
-     * DataSourceInitializer(); initializer.setDataSource(dataSource);
-     * initializer.setDatabasePopulator(databasePopulator()); return
-     * initializer; }
-     *
-     * private DatabasePopulator databasePopulator() { final
-     * ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-     * populator.addScript(schemaScript); return populator; }
-     *
-     * @Bean public DataSource dataSource() { final DriverManagerDataSource
-     * dataSource = new DriverManagerDataSource();
-     * dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-     * dataSource.setUrl(env.getProperty("jdbc.url"));
-     * dataSource.setUsername(env.getProperty("jdbc.user"));
-     * dataSource.setPassword(env.getProperty("jdbc.pass")); return dataSource;
-     * }
-     *
-     * @Bean public TokenStore tokenStore() { return new
-     * JdbcTokenStore(dataSource()); }
-     */
+	@Bean
+	public DataSource dataSource() {
+		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.user"));
+		dataSource.setPassword(env.getProperty("jdbc.pass"));
+		return dataSource;
+	}
+
+	@Bean
+	public TokenStore tokenStore() {
+		return new JdbcTokenStore(dataSource());
+	}
+
 }
